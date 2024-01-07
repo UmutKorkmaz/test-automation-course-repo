2e057a2039fb6e26724de1e16e7e5299f529984d
Umut Korkmaz <umutkorkmaz@outlook.com.tr>
2024-01-08 00:11:04 +0300

    fix: UniqueCharacterFinder

--- UniqueCharacterFinder/UniqueCharacterFinder.java
+++ UniqueCharacterFinder/UniqueCharacterFinder.java
@@ -4,17 +4,20 @@ import java.util.Set;
 public class UniqueCharacterFinder {
 
     public static Set<Character> findUniqueCharacters(String[] texts) {
-        Set<Character> foundCharacters = new HashSet<>();
+        Set<String> words = new HashSet<>();
         Set<Character> uniqueCharacters = new HashSet<>();
         int wordsFound = 0;
 
         for (String text : texts) {
             for (String word : text.split("\\s+")) {
-                if (wordsFound < 2 && allCharsRepeat(texts, word)) {
-                    for (char c : word.toCharArray()) {
-                        uniqueCharacters.add(c);
+                if (!words.add(word)) {
+                    // Eğer kelime daha önce eklendiyse ve ikinci kez bulunmuşsa:
+                    if (wordsFound < 2) {
+                        for (char c : word.toCharArray()) {
+                            uniqueCharacters.add(c);
+                        }
+                        wordsFound++;
                     }
-                    wordsFound++;
                 }
             }
         }
@@ -22,25 +25,8 @@ public class UniqueCharacterFinder {
         return uniqueCharacters;
     }
 
-    private static boolean allCharsRepeat(String[] texts, String word) {
-        for (char c : word.toCharArray()) {
-            int count = 0;
-            for (String text : texts) {
-                for (char textChar : text.toCharArray()) {
-                    if (textChar == c) {
-                        count++;
-                    }
-                }
-            }
-            if (count < 2) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public static void main(String[] args) {
-        String[] texts = {"hello world", "good day", "repeatable characters", "final test"};
+        String[] texts = {"hello", "world", "find", "world", "find", "good", "hello"};
         Set<Character> result = findUniqueCharacters(texts);
         System.out.println("Unique characters: " + result);
     }
